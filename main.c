4529c43db67be1d172f64a5ff7d3aa2730efac9a
khaledwesslati <khaledwesslati@esprit.tn>
2020-10-29 12:21:28 +0100

    MAJ main

---
+++ main.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+
+
+int main()
+{ 
+int a,b,c;
+printf ("donner le valeur a = %d ",a);
+printf ("donner le valeur b = %d ",b);
+c=a*b;
+printf ("donner le valeur c = %d ",c);
+return 0;
+
